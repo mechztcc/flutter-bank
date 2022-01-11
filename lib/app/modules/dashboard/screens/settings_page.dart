b27ada744d17a6e8474c9355cbf98ea90378cc39
mechztcc <dojathan@gmail.com>
2022-01-11 11:08:05 -0300

    settings page

--- lib/app/modules/dashboard/screens/settings_page.dart
+++ lib/app/modules/dashboard/screens/settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class SettingsPage extends StatefulWidget {
   final String title;
@@ -13,7 +14,47 @@ class SettingsPageState extends State<SettingsPage> {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 40,
+                ),
+                child: ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.transparent,
+                  ),
+                  onPressed: () {
+                    Modular.to.pushReplacementNamed('/dashboard');
+                  },
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    'Voltar',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                child: Text(
+                  'Configurações da conta',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
